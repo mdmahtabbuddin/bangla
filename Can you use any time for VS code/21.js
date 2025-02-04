4410b848e36d9db57b83dff1fa0c9792d99edfda
Mahtab <mahfuzamsiddika@gmail.com>
2025-02-04 18:43:28 +0600

    Add files via upload

---
+++ Can you use any time for VS code/21.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    alert("🔸স্বাগতম!🔸সব গুলো পয়েন্ট প্লিজ ক্লিক করুন");
+  };
+
+function fun1() {
+    // You can replace this URL with whatever link you want
+    window.location.href = "http://127.0.0.1:5500/1.html";  // Change to your desired URL
+  }
+
+  function fun2() {
+    // You can replace this URL with whatever link you want
+    window.location.href = "http://127.0.0.1:5500/2.html";  // Change to your desired URL
+  }
+
+  function fun3() {
+    // You can replace this URL with whatever link you want
+    window.location.href = "http://127.0.0.1:5500/3.html";  // Change to your desired URL
+  }
